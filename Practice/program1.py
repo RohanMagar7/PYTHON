2571363f71ad9c5a6ce27a257475ff4e1533f0de
RohanMagar7 <rmtechyt@gmail.com>
2025-01-03 22:59:42 +0530

    String in python

--- Practice/program1.py
+++ Practice/program1.py
@@ -31,3 +31,99 @@
 
 
 # """ Lecture : 2 practice sets """
+
+
+# # 1 Strings 
+# # String is data type the stores a swquence of characters
+
+# str1 = "Hello"
+# str2 = 'World'
+# str3 = """Hello World"""
+# print(str1)
+# print(str2)
+# print(str3)
+
+# # # String Operations
+# # 1 String Concatenation
+# # Concatenation is the process of combining two strings
+
+# str1 = "Hello"
+# str2 = "World"
+# str3 = str1+str2
+# print(str3)
+
+# # 2 String Repetition
+# # Repetition is the process of repeating a string multiple times
+
+# str1 = "Hello"
+# str2 = str1*3
+# print(str2)
+
+# # 3 String Slicing
+# # Slicing is the process of extracting a part of a string
+
+# str1 = "Hello World"
+# print(str1[0:5])
+# print(str1[5:11])
+
+# # 4 String Length
+# # Length is the number of characters in a string
+
+# str1 = "Hello World"
+# print(len(str1))
+
+
+# # 5 String Membership
+# # Membership is the process of checking if a string is present in another string
+
+# str1 = "Hello World"
+# print("Hello" in str1)
+# print("World" in str1)
+# print("Hello" not in str1)
+# print("World" not in str1)
+
+# # 6 String Comparison
+# # Comparison is the process of comparing two strings
+
+# str1 = "Hello"
+# str2 = "World"
+# print(str1==str2)
+# print(str1!=str2)
+# print(str1>str2)
+# print(str1<str2)
+# print(str1>=str2)
+# print(str1<=str2)
+
+# 7 String Formatting
+# Formatting is the process of formatting a string
+
+str1 = "Hello"
+str2 = "World"
+print("Hello %s"%str2)
+print("Hello %s"%str1)
+print("Hello %s %s"%(str1,str2))
+
+# 8 String Methods
+# Methods are functions that are associated with objects
+
+str1 = "Hello World"
+print(str1.upper())
+print(str1.lower())
+print(str1.title())
+print(str1.capitalize())
+print(str1.swapcase())
+print(str1.find("World"))
+print(str1.replace("World","India"))
+print(str1.split(" "))
+print(str1.strip())
+print(str1.lstrip())
+print(str1.rstrip())
+print(str1.isalnum())
+print(str1.isalpha())
+print(str1.isdigit())
+print(str1.isspace())
+print(str1.startswith("Hello"))
+print(str1.endswith("World"))
+
+# 9 Escape Sequences
+# Escape sequences are used to represent special characters
---
+++ Practice/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+# 7 String Formatting
+# Formatting is the process of formatting a string
+
+str1 = "Hello"
+str2 = "World"
+print("Hello %s"%str2)
+print("Hello %s"%str1)
+print("Hello %s %s"%(str1,str2))
